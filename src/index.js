c31fba75e93ecc7ad5078fdab556bf1928cad67b
carloshnunes <110049855+carloshnunes@users.noreply.github.com>
2024-05-29 09:37:49 -0300

    primeiro commit da api

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+mongoose.connect(
+  "mongodb+srv://carloshnunes:<Ghyrmj5C8KviEX4PT>@cluster0.m1wkvt8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+const Film = mongoose.model("Film", {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer: String,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+
+  await film.save();
+  res.send(film);
+});
+app.listen(port, () => {
+  console.log("App running");
+});
